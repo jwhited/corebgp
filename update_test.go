db6a9fb2537ba83f4783175fa91c46e5c0ef1221
Jordan Whited <jordan@jordanwhited.com>
2023-02-11 13:55:25 -0800

    implement add-path decoding
    Signed-off-by: Jordan Whited <jordan@jordanwhited.com>

--- update_test.go
+++ update_test.go
@@ -10,14 +10,19 @@ import (
 )
 
 type updateMessageForTests struct {
-	withdrawn     []netip.Prefix
-	origin        uint8
-	asPath        []uint32
-	nextHop       netip.Addr
-	nlri          []netip.Prefix
-	ipv6NextHops  []netip.Addr
-	ipv6NLRI      []netip.Prefix
-	ipv6Withdrawn []netip.Prefix
+	addPath              bool
+	withdrawn            []netip.Prefix
+	addPathWithdrawn     []AddPathPrefix
+	origin               uint8
+	asPath               []uint32
+	nextHop              netip.Addr
+	nlri                 []netip.Prefix
+	addPathNLRI          []AddPathPrefix
+	ipv6NextHops         []netip.Addr
+	addPathIPv6NLRI      []AddPathPrefix
+	ipv6NLRI             []netip.Prefix
+	addPathIPv6Withdrawn []AddPathPrefix
+	ipv6Withdrawn        []netip.Prefix
 }
 
 func newPathAttrsDecodeFn() func(m *updateMessageForTests, code uint8, flags PathAttrFlags, b []byte) error {
@@ -28,12 +33,20 @@ func newPathAttrsDecodeFn() func(m *updateMessageForTests, code uint8, flags Pat
 				if err != nil {
 					return err
 				}
-				prefixes, err := DecodeMPIPv6Prefixes(nlri)
-				if err != nil {
-					return err
+				if m.addPath {
+					prefixes, err := DecodeMPIPv6AddPathPrefixes(nlri)
+					if err != nil {
+						return err
+					}
+					m.addPathIPv6NLRI = prefixes
+				} else {
+					prefixes, err := DecodeMPIPv6Prefixes(nlri)
+					if err != nil {
+						return err
+					}
+					m.ipv6NLRI = prefixes
 				}
 				m.ipv6NextHops = nhs
-				m.ipv6NLRI = prefixes
 			}
 			return nil
 		},
@@ -41,11 +54,19 @@ func newPathAttrsDecodeFn() func(m *updateMessageForTests, code uint8, flags Pat
 	unreachDecodeFn := NewMPUnreachNLRIDecodeFn[*updateMessageForTests](
 		func(m *updateMessageForTests, afi uint16, safi uint8, withdrawn []byte) error {
 			if afi == AFI_IPV6 && safi == SAFI_UNICAST {
-				prefixes, err := DecodeMPIPv6Prefixes(withdrawn)
-				if err != nil {
-					return err
+				if m.addPath {
+					prefixes, err := DecodeMPIPv6AddPathPrefixes(withdrawn)
+					if err != nil {
+						return err
+					}
+					m.addPathIPv6Withdrawn = prefixes
+				} else {
+					prefixes, err := DecodeMPIPv6Prefixes(withdrawn)
+					if err != nil {
+						return err
+					}
+					m.ipv6Withdrawn = prefixes
 				}
-				m.ipv6Withdrawn = prefixes
 			}
 			return nil
 		},
@@ -99,6 +120,15 @@ func FuzzUpdateDecoder_Decode(f *testing.F) {
 		0x08, 0x0a, // nlri 10.0.0.0/8
 	})
 	f.Add([]byte{
+		0x00, 0x07, // withdrawn routes length
+		0x00, 0x00, 0x00, 0x01, 0x10, 0x0a, 0x00, // withdrawn id 1 10.0.0.0/16
+		0x00, 0x14, // total path attribute length
+		0x40, 0x01, 0x01, 0x01, // origin egp
+		0x40, 0x02, 0x06, 0x02, 0x01, 0x00, 0x00, 0xfd, 0xea, // as_path 65002
+		0x40, 0x03, 0x04, 0xc0, 0x00, 0x02, 0x02, // next_hop 192.0.2.2
+		0x00, 0x00, 0x00, 0x02, 0x08, 0x0a, // nlri id 2 10.0.0.0/8
+	})
+	f.Add([]byte{
 		0x00, 0x00, // withdrawn routes length
 		0x00, 0x3f, // total path attribute length
 		// extended len MP_REACH_NLRI 2001:db8::/64 nhs 2001:db8::2 & fe80::42:c0ff:fe00:202
@@ -128,17 +158,45 @@ func FuzzUpdateDecoder_Decode(f *testing.F) {
 	})
 }
 
+func newWithdrawnRoutesDecodeFn() DecodeFn[*updateMessageForTests] {
+	fn := NewWithdrawnRoutesDecodeFn[*updateMessageForTests](func(u *updateMessageForTests, p []netip.Prefix) error {
+		u.withdrawn = p
+		return nil
+	})
+	apFn := NewWithdrawnAddPathRoutesDecodeFn[*updateMessageForTests](func(u *updateMessageForTests, a []AddPathPrefix) error {
+		u.addPathWithdrawn = a
+		return nil
+	})
+	return func(u *updateMessageForTests, b []byte) error {
+		if u.addPath {
+			return apFn(u, b)
+		}
+		return fn(u, b)
+	}
+}
+
+func newNLRIDecodeFn() DecodeFn[*updateMessageForTests] {
+	fn := NewNLRIDecodeFn[*updateMessageForTests](func(u *updateMessageForTests, p []netip.Prefix) error {
+		u.nlri = p
+		return nil
+	})
+	apFn := NewNLRIAddPathDecodeFn[*updateMessageForTests](func(u *updateMessageForTests, a []AddPathPrefix) error {
+		u.addPathNLRI = a
+		return nil
+	})
+	return func(u *updateMessageForTests, b []byte) error {
+		if u.addPath {
+			return apFn(u, b)
+		}
+		return fn(u, b)
+	}
+}
+
 func TestUpdateDecoder_Decode(t *testing.T) {
 	ud := NewUpdateDecoder[*updateMessageForTests](
-		NewWithdrawnRoutesDecodeFn(func(m *updateMessageForTests, r []netip.Prefix) error {
-			m.withdrawn = r
-			return nil
-		}),
+		newWithdrawnRoutesDecodeFn(),
 		newPathAttrsDecodeFn(),
-		NewNLRIDecodeFn(func(m *updateMessageForTests, r []netip.Prefix) error {
-			m.nlri = r
-			return nil
-		}),
+		newNLRIDecodeFn(),
 	)
 
 	t.Run("valid data", func(t *testing.T) {
@@ -150,32 +208,88 @@ func TestUpdateDecoder_Decode(t *testing.T) {
 			{
 				name: "IPv4 withdrawn and IPv4 nlri",
 				toDecode: []byte{
-					0x00, 0x03, // withdrawn routes length
+					0x00, 0x05, // withdrawn routes length
 					0x10, 0x0a, 0x00, // withdrawn 10.0.0.0/16
+					0x08, 0x01, // withdrawn 1.0.0.0/8
 					0x00, 0x14, // total path attribute length
 					0x40, 0x01, 0x01, 0x01, // origin egp
 					0x40, 0x02, 0x06, 0x02, 0x01, 0x00, 0x00, 0xfd, 0xea, // as_path 65002
 					0x40, 0x03, 0x04, 0xc0, 0x00, 0x02, 0x02, // next_hop 192.0.2.2
 					0x08, 0x0a, // nlri 10.0.0.0/8
+					0x18, 0x01, 0x00, 0x00, // nlri 1.0.0.0/24
 				},
 				want: &updateMessageForTests{
-					withdrawn: []netip.Prefix{netip.MustParsePrefix("10.0.0.0/16")},
-					origin:    1,
-					asPath:    []uint32{65002},
-					nextHop:   netip.MustParseAddr("192.0.2.2"),
-					nlri:      []netip.Prefix{netip.MustParsePrefix("10.0.0.0/8")},
+					withdrawn: []netip.Prefix{
+						netip.MustParsePrefix("10.0.0.0/16"),
+						netip.MustParsePrefix("1.0.0.0/8"),
+					},
+					origin:  1,
+					asPath:  []uint32{65002},
+					nextHop: netip.MustParseAddr("192.0.2.2"),
+					nlri: []netip.Prefix{
+						netip.MustParsePrefix("10.0.0.0/8"),
+						netip.MustParsePrefix("1.0.0.0/24"),
+					},
 				},
 			},
 			{
-				name: "MP_UNREACH_NLRI IPv6 prefix",
+				name: "add-path IPv4 withdrawn and IPv4 nlri",
+				toDecode: []byte{
+					0x00, 0x0f, // withdrawn routes length
+					0x00, 0x00, 0x00, 0x01, 0x10, 0x0a, 0x00, // withdrawn id 1 10.0.0.0/16
+					0x00, 0x00, 0x00, 0x02, 0x18, 0x0a, 0x00, 0x00, // withdrawn id 2 10.0.0.0/24
+					0x00, 0x14, // total path attribute length
+					0x40, 0x01, 0x01, 0x01, // origin egp
+					0x40, 0x02, 0x06, 0x02, 0x01, 0x00, 0x00, 0xfd, 0xea, // as_path 65002
+					0x40, 0x03, 0x04, 0xc0, 0x00, 0x02, 0x02, // next_hop 192.0.2.2
+					0x00, 0x00, 0x00, 0x02, 0x08, 0x0a, // nlri id 2 10.0.0.0/8
+				},
+				want: &updateMessageForTests{
+					addPath: true,
+					addPathWithdrawn: []AddPathPrefix{
+						{netip.MustParsePrefix("10.0.0.0/16"), 1},
+						{netip.MustParsePrefix("10.0.0.0/24"), 2},
+					},
+					origin:  1,
+					asPath:  []uint32{65002},
+					nextHop: netip.MustParseAddr("192.0.2.2"),
+					addPathNLRI: []AddPathPrefix{
+						{netip.MustParsePrefix("10.0.0.0/8"), 2},
+					},
+				},
+			},
+			{
+				name: "MP_UNREACH_NLRI IPv6 prefixes",
 				toDecode: []byte{
 					0x00, 0x00, // withdrawn routes length
-					0x00, 0x09, // total path attribute length
-					0x90, 0x0f, 0x00, 0x05, 0x00, 0x02, 0x01, 0x07, 0xfc, // extended len IPv6 MP_UNREACH_NLRI fc00::/7
+					0x00, 0x0b, // total path attribute length
+					0x90, 0x0f, 0x00, 0x07, 0x00, 0x02, 0x01, 0x07, 0xfc, 0x07, 0xfa, // extended len IPv6 MP_UNREACH_NLRI fc00::/7 fa00::/7
 				},
 				want: &updateMessageForTests{
 					ipv6Withdrawn: []netip.Prefix{
 						netip.MustParsePrefix("fc00::/7"),
+						netip.MustParsePrefix("fa00::/7"),
+					},
+				},
+			},
+			{
+				name: "add-path MP_UNREACH_NLRI IPv6 prefixes",
+				toDecode: []byte{
+					0x00, 0x00, // withdrawn routes length
+					0x00, 0x13, // total path attribute length
+					0x90, 0x0f, 0x00, 0x0f, 0x00, 0x02, 0x01, 0x00, 0x00, 0x00, 0x01, 0x07, 0xfc, 0x00, 0x00, 0x00, 0x02, 0x07, 0xfa, // extended len IPv6 MP_UNREACH_NLRI id 1 fc00::/7 id 2 fa00::/7
+				},
+				want: &updateMessageForTests{
+					addPath: true,
+					addPathIPv6Withdrawn: []AddPathPrefix{
+						{
+							Prefix: netip.MustParsePrefix("fc00::/7"),
+							ID:     1,
+						},
+						{
+							Prefix: netip.MustParsePrefix("fa00::/7"),
+							ID:     2,
+						},
 					},
 				},
 			},
@@ -189,12 +303,59 @@ func TestUpdateDecoder_Decode(t *testing.T) {
 				want: &updateMessageForTests{},
 			},
 			{
+				name: "add-path MP_REACH_NLRI IPv6 prefixes",
+				toDecode: []byte{
+					0x00, 0x00, // withdrawn routes length
+					0x00, 0x50, // total path attribute length
+					// extended len MP_REACH_NLRI
+					0x90, 0x0e, 0x00, 0x3F, 0x00, 0x02, 0x01, 0x20,
+					// nh 2001:db8::2
+					0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02,
+					// nh fe80::42:c0ff:fe00:202
+					0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x42, 0xc0, 0xff, 0xfe, 0x00, 0x02, 0x02,
+					0x00, // reserved
+					// id 1 2001:db8::/64
+					0x00, 0x00, 0x00, 0x01, 0x40, 0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00,
+					// id 2 2001:db9::/64
+					0x00, 0x00, 0x00, 0x02, 0x40, 0x20, 0x01, 0x0d, 0xb9, 0x00, 0x00, 0x00, 0x00,
+					0x40, 0x01, 0x01, 0x00, // origin igp
+					0x40, 0x02, 0x06, 0x02, 0x01, 0x00, 0x00, 0xfd, 0xea, // as_path 65002
+				},
+				want: &updateMessageForTests{
+					addPath: true,
+					asPath:  []uint32{65002},
+					addPathIPv6NLRI: []AddPathPrefix{
+						{
+							Prefix: netip.MustParsePrefix("2001:db8::/64"),
+							ID:     1,
+						},
+						{
+							Prefix: netip.MustParsePrefix("2001:db9::/64"),
+							ID:     2,
+						},
+					},
+					ipv6NextHops: []netip.Addr{
+						netip.MustParseAddr("2001:db8::2"),
+						netip.MustParseAddr("fe80::42:c0ff:fe00:202"),
+					},
+				},
+			},
+			{
 				name: "MP_REACH_NLRI IPv6 prefix",
 				toDecode: []byte{
 					0x00, 0x00, // withdrawn routes length
-					0x00, 0x3f, // total path attribute length
-					// extended len MP_REACH_NLRI 2001:db8::/64 nhs 2001:db8::2 & fe80::42:c0ff:fe00:202
-					0x90, 0x0e, 0x00, 0x2e, 0x00, 0x02, 0x01, 0x20, 0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x42, 0xc0, 0xff, 0xfe, 0x00, 0x02, 0x02, 0x00, 0x40, 0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00,
+					0x00, 0x48, // total path attribute length
+					// extended len MP_REACH_NLRI
+					0x90, 0x0e, 0x00, 0x37, 0x00, 0x02, 0x01, 0x20,
+					// nh 2001:db8::2
+					0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02,
+					// nh fe80::42:c0ff:fe00:202
+					0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x42, 0xc0, 0xff, 0xfe, 0x00, 0x02, 0x02,
+					0x00, // reserved
+					// 2001:db8::/64
+					0x40, 0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00,
+					// 2001:db9::/64
+					0x40, 0x20, 0x01, 0x0d, 0xb9, 0x00, 0x00, 0x00, 0x00,
 					0x40, 0x01, 0x01, 0x00, // origin igp
 					0x40, 0x02, 0x06, 0x02, 0x01, 0x00, 0x00, 0xfd, 0xea, // as_path 65002
 				},
@@ -202,6 +363,7 @@ func TestUpdateDecoder_Decode(t *testing.T) {
 					asPath: []uint32{65002},
 					ipv6NLRI: []netip.Prefix{
 						netip.MustParsePrefix("2001:db8::/64"),
+						netip.MustParsePrefix("2001:db9::/64"),
 					},
 					ipv6NextHops: []netip.Addr{
 						netip.MustParseAddr("2001:db8::2"),
@@ -213,6 +375,7 @@ func TestUpdateDecoder_Decode(t *testing.T) {
 		for _, tt := range cases {
 			t.Run(tt.name, func(t *testing.T) {
 				m := &updateMessageForTests{}
+				m.addPath = tt.want.addPath
 				n := ud.Decode(m, tt.toDecode)
 				if n != nil {
 					t.Fatalf("error decoding: %v", n)
